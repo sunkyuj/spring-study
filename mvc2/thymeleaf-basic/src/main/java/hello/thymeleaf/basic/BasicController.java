db30d86be1bac68bd02341cbb31b7f61b7fa4c71
sunkyuj <sunkyuj12@gmail.com>
2023-04-24 16:42:53 +0900

    Thymeleaf study (attribute, block, comments, condition, each)

--- mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -95,6 +95,40 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+    @GetMapping("/comments")
+    public String comments(){
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
 
     @Data
     static class User {
